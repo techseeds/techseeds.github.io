e86f25bffe5c9c4546ba086a3d8a0113ea4f4029
Mahdi Mazaheri <me.mazaheri@gmail.com>
2016-04-30 21:21:42 +0430

    first commit

---
+++ techseeds_files/jquery.magnific-popup.min.js
@@ -0,0 +1,270 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <script src="//code.jquery.com/jquery.min.js"></script>
+
+    <title>MIT Launch Summer - Home</title>
+    <base href="http://mitlaunch.com/" />
+
+    <!-- Fav Icons -->
+    <link rel="icon" href="assets/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/favicons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="assets/favicons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/favicons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/favicons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/favicons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/favicons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/favicons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/favicons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/favicons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="assets/favicons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/favicons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/favicons/favicon-16x16.png">
+    <link rel="manifest" href="assets/favicons/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- Bootstrap Core CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Retina.js - Load first for faster HQ mobile assets/2014-2s. -->
+    <script src="assets/js/retina.min.js"></script>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+    <!-- Default Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,600,500,700,800,900' rel='stylesheet' type='text/css'>
+
+    <!-- Modern Style Fonts (Include these is you are using body.modern!) -->
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Cardo:400,400italic,700' rel='stylesheet' type='text/css'>
+
+    <!-- Vintage Style Fonts (Include these if you are using body.vintage!) -->
+    <link href='http://fonts.googleapis.com/css?family=Sanchez:400italic,400' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Cardo:400,400italic,700' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="assets/css/owl.carousel.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/owl.theme.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/owl.transitions.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/magnific-popup.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/jquery.fs.wallpaper.css" rel="stylesheet" type="text/css">
+
+    <!-- Timeline CSS -->
+    <link href="assets/css/timeline.css" rel="stylesheet" type="text/css">
+
+    <!-- IE8 support for HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<link href="assets/css/vitality-orange-full.css" rel="stylesheet" type="text/css">
+
+<body id="page-top">
+    <!-- Navigation -->
+    <!-- Note: navbar-default and navbar-inverse are both supported with this theme. -->
+    <nav class="navbar navbar-inverse navbar-fixed-top navbar-expanded">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="http://mitlaunch.com/">
+                    <img src="assets/logo-nav@2x.png" class="img-responsive" alt="">
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="about.html">About Us</a>
+                    </li>
+                    <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Launch Summer <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="program.html">Program Details</font></a></li>
+                            <li><a href="faq.html">FAQ</font></a></li>
+                            <li><a href="alumni.html">Launch Alumni</font></a></li>
+                            <li><a href="companies/">Past Companies</font></a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="https://launch.fluidreview.com/" target="_blank">Apply!</font></a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="https://www.edx.org/course/becoming-entrepreneur-mitx-launch-x" target="_blank">LaunchX</a>
+                    </li>
+                    <li>
+                        <a href="clubs/" target="_blank">Launch Clubs</a>
+                    </li>
+                    <li>
+                        <a href="https://launchsummer.wordpress.com/" target="_blank">Blog</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        <!-- /.container -->
+    </nav>
+     <header style="background-image: url('assets/bg-header-v.png');" id="starfield">
+        <div class="intro-content">
+
+                <img src="assets/launch-logo.png" class="img-responsive img-centered" alt="Launch Logo">
+                <img src="assets/mit-launch.png" class="img-responsive img-centered" alt="MIT Launch">
+                <hr class="colored">
+                <div class="brand-name-subtext"> High School Entrepreneurship Programs</div>
+                <br>
+                <div class="brand-name-subtext">Unleash your inner entrepreneur<br></div>
+        </div>
+        <div class="scroll-down">
+            <a class="btn" href="#aboutWhatWeDo"><i class="fa fa-angle-down fa-fw"></i></a>
+        </div>
+    </header>
+   <section class="bg-gray" id="aboutWhatWeDo">
+        <div class="container-fluid"> 
+            <div class="row text-center">
+                    <h2>What We Do</h2>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <div class="about-content"><center>
+                        <a href=program.html target="_blank" class="btn btn-block orange-divisor"><i class="fa fa-rocket fa-5x orange"></i></a>
+                        <h3>Summer Program</h3>
+<a href="program.html">www.launchsummer.org</a><br><br>
+                        <p>MIT Launch Summer brings together high school students from all over the US and world each summer to MIT campus to become entrepreneurs in a 4-week program.  This experience connects students to many of the best entrepreneurial networks and leaders across campus for instruction and best practices, engraining them in the MIT culture of Mens et Manus - mind and hand.<br>Admissions decisions for summer 2016 will be emailed on April 15.
+</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <div class="about-content"><center>
+                        <a href="http://www.mitlaunchclubs.com" target="_blank" class="btn btn-block orange-divisor"><i class="fa fa-space-shuttle fa-5x orange"></i></a>
+                        <h3>Clubs</h3>
+<a href="http://mitlaunchclubs.com" target="_blank">www.mitlaunchclubs.com</a><br><br>
+
+                        <p>Through Launch Clubs, school entrepreneurship clubs are being started at schools around the world, with teams becoming entrepreneurs and starting real companies. The first year involved around 20 schools of Launch alumni with about 100 companies started. Finalists were invited to a mentorship and pitch competition at MIT in the spring of 2015. Launch Clubs has expanded to almost 100 schools this year.</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <div class="about-content"><center>
+                        <a href="https://www.edx.org/course/becoming-entrepreneur-mitx-launch-x" target="_blank" class="btn btn-block orange-divisor"><i class="fa fa-laptop fa-5x orange"></i></a>
+                        <h3>Online Program</h3>
+<a href="https://www.edx.org/course/becoming-entrepreneur-mitx-launch-x" target="_blank">Register for FREE now!</a><br><br>
+                        <p>LaunchX is a new online course aimed at guiding first-time entrepreneurs through the steps of starting a company, helping to overcome the hurdles to making that first step to launching something new. The course registration has just opened and is available now on edX / MITx. <a href="https://www.edx.org/course/becoming-entrepreneur-mitx-launch-x" target="_blank">Register for FREE here.</a><br><br>
+</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+ <section id="about-us" >
+            <div class="row">
+                <div class="col-lg-12">
+<center>
+                    <h2>Who We Are</h2>
+                    <hr class="colored">
+                    <p>MIT Launch is a part of the <a href="https://entrepreneurship.mit.edu/" target="_blank">Martin Trust Center for MIT Entrepreneurship</a>.  That means a few things:</p></center>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <div class="about-content"><center>
+                         <h3>MIT Standards</h3>
+                        <p>We follow MIT’s Standards of Excellence and Rigor in education, advising and practical experience. True to the motto of MIT, in all of our courses and throughout our activities, we operate on a hybrid model that fuses academic and practitioner perspectives - Mens et Manus.
+Plus, we are an honest broker: No one at MIT Launch will ever take a part of the new companies that we nurture and assist.</p>
+                                    <a href="http://web.mit.edu/" target="_blank"><img class="lead" src="assets/mit-logo.gif" class="img-responsive" alt=""></a>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <div class="about-content"><center>
+                         <h3>MIT Network</h3>
+                        <p>The <a href="https://entrepreneurship.mit.edu/" target="_blank">Martin Trust Center for MIT Entrepreneurship</a> works closely with other MIT departments, labs, centers and groups to connect students with the best entrepreneurship programming across the Institute.  Plus, it hosts a family of programs that Launch is proud to be a part of.</p>
+                                        <a href="http://reap.mit.edu/" target="_blank"><img class="lead"src="assets/reap.png" class="img-responsive img-network" alt=""></a>
+                                    <a href="https://entrepreneurship.mit.edu/accelerator/" target="_blank"><img class="lead"src="assets/gfsa.jpg" class="img-responsive img-network" alt=""></a>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <div class="about-content"><center>
+                        <h3>MIT Growth</h3>
+                        <p>We will continually try new programs and activities to be able to have a bigger reach and impact through entrepreneurship education. If we fail, we learn in the process. When we succeed, we aim to scale rapidly. We are educators. Our goal is your success.</p>
+                            <img src="assets/LaunchX.png" class="img-responsive img-centered" alt="" height="250" width="250">
+                </div>
+            </div>
+        </div>
+        </div>
+    </section> 
+
+<a href="http://goo.gl/forms/ecHrGX27E1" target="_blank" class="btn btn-block btn-full-width orange-divisor"><b>Would you like to volunteer to MENTOR teams this summer?  Fill out THIS FORM</b></a>
+
+   <footer class="footer" style="background-image: url('assets/mit_dome.jpg')">
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-md-6 contact-details">
+                    <h4><i class="fa fa-envelope"></i> MIT Launch Newsletter</h4>
+                    <p><a href="http://app.streamsend.com/public/9goxom3g2t/bNM/subscribe" target="_blank">Join our mailing list</a>
+                    </p>
+                </div>
+                <div class="col-md-6 contact-details">
+                    <h4><i class="fa fa-envelope"></i> General Info</h4>
+                    <p><a href="mailto:info@launchsummer.org">info@launchsummer.org</a>
+                    </p>
+                </div>
+            </div>
+            <div class="row social">
+                <div class="col-lg-12">
+                    <ul class="list-inline">
+                        <li><a href="http://facebook.com/launchsummer"><i class="fa fa-facebook fa-fw fa-2x"></i></a>
+                        </li>
+                        <li><a href="http://twitter.com/launchsummer"><i class="fa fa-twitter fa-fw fa-2x"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row copyright">
+                <div class="col-lg-12">
+                    <p class="small">&copy; 2015 Launch Summer Program, Inc. All rights reserved.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+   <!-- Core Scripts -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugin Scripts -->
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <script src="assets/js/classie.js"></script>
+    <script src="assets/js/cbpAnimatedHeader.js"></script>
+    <script src="assets/js/owl.carousel.js"></script>
+    <script src="assets/js/jquery.magnific-popup/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/jquery.fs.wallpaper.js"></script>
+    <script src="assets/js/jqBootstrapValidation.js"></script>
+
+    <!-- Vitality Theme Script -->
+    <script src="assets/js/vitality.js"></script>
+
+    <!-- Timeline Script -->
+    <script src="assets/js/timeline.js"></script>
+
+    <!-- Starfield fun! -->
+    <script type='text/javascript' src='assets/js/jquery.particleground.js'></script>
+    <script type='text/javascript' src='assets/js/starfield.js'></script>
+</body>
+</html>
